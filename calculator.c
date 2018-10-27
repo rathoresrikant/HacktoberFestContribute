b82d287cc6237135c4afee848aa87585b05780e2
JustKshitijD <44445148+JustKshitijD@users.noreply.github.com>
2018-10-27 15:52:15 +0530

    entered default case
    added % operator and in case of wrong operator,notify the user

--- calculator.c
+++ calculator.c
@@ -1,33 +1,38 @@
-#include<stdio.h>
-
-int main(void)
+#include<bits/stdc++.h>
+using namespace std;
+int main()
 {
   char operator;
     double n1,n2;
 
-    printf("Enter an operator (+, -, *,/): ");
-    scanf("%c", &operator);
+    printf("Enter an operator (+, -, *,/,%): ");
+    
 
     printf("Enter two numbers: ");
-    scanf("%lf  %lf",&n1, &n2);
+    cin>>n1>>n2;
 
   switch(operator)
   {
    case '+':
-        printf("%lf + %lf = %lf ", n1, n2, n1 + n2);
+        cout<<n1<<"+"<<n2<<"="<<n1+n2;
         break;
 
     case '-':
-        printf("%lf - %lf = %lf ", n1, n2, n1 - n2);
+          cout<<n1<<"-"<<n2<<"="<<n1-n2;
         break;
 
     case '*':
-        printf("%lf * %lf = %lf ", n1, n2, n1 * n2);
+        cout<<n1<<"*"<<n2<<"="<<n1*n2;
         break;
 
     case '/':
-        printf("%lf / %lf = %lf ", n1, n2, n1 / n2);
+          cout<<n1<<"/"<<n2<<"="<<n1/n2;
         break;
+    case '%':
+      cout<<n1<<"%"<<n2<<"="<<n1%n2;
+      break;
+    default:
+      cout<<"Please enter one of the operators +,-,*,/,%";
   }
   printf("\n");
   return 0;
