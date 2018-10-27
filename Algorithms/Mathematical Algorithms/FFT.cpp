a049e3978d0b48a4e65a37177eb251582f0272c3
z3r0dmg <30744779+z3r0dmg@users.noreply.github.com>
2018-10-27 17:23:18 +0530

    Fast Fourier Transform
    Program to multiply 2 polynomials in O(nlog(n))

---
+++ Algorithms/Mathematical Algorithms/FFT.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using cd = complex<double>;
+const double PI = acos(-1);
+
+void fft(vector<cd> & a, bool invert) {
+    int n = a.size();
+    if (n == 1)
+        return;
+
+    vector<cd> a0(n / 2), a1(n / 2);
+    for (int i = 0; 2 * i < n; i++) {
+        a0[i] = a[2*i];
+        a1[i] = a[2*i+1];
+    }
+    fft(a0, invert);
+    fft(a1, invert);
+
+    double ang = 2 * PI / n * (invert ? -1 : 1);
+    cd w(1), wn(cos(ang), sin(ang));
+    for (int i = 0; 2 * i < n; i++) {
+        a[i] = a0[i] + w * a1[i];
+        a[i + n/2] = a0[i] - w * a1[i];
+        if (invert) {
+            a[i] /= 2;
+            a[i + n/2] /= 2;
+        }
+        w *= wn;
+    }
+}
+
+vector<int> multiply(vector<int> const& a, vector<int> const& b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<int> result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    return result;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+    vector<int> a,b,c;
+    int x;
+
+    for(int i=0; i<n+1; i++){
+        cin>>x;
+        a.pb(x);
+    }
+    for(int i=0; i<n+1; i++){
+        cin>>x;
+        b.pb(x);
+    }
+
+    c=multiply(a,b);
+
+    for(int i=0; i<2*n+1; i++){
+        cout<<c[i]<<" ";
+    }
+
+    cout<<'\n';
+}
