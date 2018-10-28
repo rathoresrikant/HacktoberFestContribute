7e6607a8373ea0fdf4a95175790bc074209eac5c
Nikhil <rangineninikhil12345@gmail.com>
2018-10-28 16:04:43 +0530

    hai

---
+++ h.java
@@ -0,0 +1,14 @@
+class StringReverse
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+
+		char[] c=s.toCharArray();
+		for(int i=c.length;i>=1;i–)
+		{
+			System.out.print(c[i-1]);
+		}
+	}
+}
