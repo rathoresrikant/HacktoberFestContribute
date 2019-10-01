d9c7d88fd0ce3a3fbba8bf93f23ed4b78b7a6a4b
Anshul Khattar <anshul070300@gmail.com>
2019-10-02 02:00:24 +0530

    Print first 10 Catalan Numbers

---
+++ Algorithms/CatalanNumber.java
@@ -0,0 +1,26 @@
+
+public class CatalanNumber {
+	
+	  static int catalanDP(int n) {
+	        int catalan[] = new int[n + 2]; 
+	
+	        catalan[0] = 1; 
+	        catalan[1] = 1; 
+	   
+	        for (int i = 2; i <= n; i++) { 
+	            catalan[i] = 0; 
+	            for (int j = 0; j < i; j++) { 
+	                catalan[i] += catalan[j] * catalan[i - j - 1]; 
+	            } 
+	        }   
+	        return catalan[n]; 
+	    } 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 10; i++) { 
+            System.out.print(catalanDP(i) + " "); 
+        }
+	}
+
+}
