4894c6569af694e09eb9875a5fa49c95e18021e3
pratyushggoyal <56029995+pratyushggoyal@users.noreply.github.com>
2019-10-01 19:12:48 +0530

    Update SwapNo.c

--- SwapNo.c
+++ SwapNo.c
@@ -3,9 +3,9 @@
 int main() 
 { 
   int x , y; 
-  printf("Enter the value of x and y:");
-  scanf("%d%d",&x,&y);
-
+  printf("Enter the value of x and y:"); //Taking the value of the numbers.
+  scanf("%d%d",&x,&y);//Reading value
+s
   // Code to swap 'x' and 'y' 
   x = x + y;   
   y = x - y;   
