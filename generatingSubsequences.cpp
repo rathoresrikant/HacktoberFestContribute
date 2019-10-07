e5a716b5ccbf208406035f89f82375cc8784cb5f
Nikhil Agarwal <nikhilag@iitk.ac.in>
2019-10-07 16:38:44 +0530

    Update generatingSubsequences.cpp

--- generatingSubsequences.cpp
+++ generatingSubsequences.cpp
@@ -50,10 +50,10 @@ int main(){
  
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-	#ifndef ONLINE_JUDGE
-		freopen("input.txt", "r", stdin);
-		freopen("output.txt", "w", stdout);
-	#endif
+// 	#ifndef ONLINE_JUDGE
+// 		freopen("input.txt", "r", stdin);
+// 		freopen("output.txt", "w", stdout);
+// 	#endif
  
 	int TC = 1,t = 0;
 	// cin >> TC;
