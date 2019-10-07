5ac1251f1cff8fb2855e4b58659498d1f7be1203
AnushaKittu <44207778+AnushaKittu@users.noreply.github.com>
2019-10-07 18:47:14 +0530

    Update fib.cpp

--- fib.cpp
+++ fib.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-
-// Return nth fibonacci value using recursion
-int fib(int n) {
+using namespace std;
+int fib(int n) 
+{
   
   if(n <= 1)
     return n;
+return fib(n - 1) + fib(n - 2);
+}
 
-  return fib(n - 1) + fib(n - 2);
-} // end function fib 
-
-int main() {
-
-  std::cout << fib(6) << std::endl;
-
+int main() 
+{
+  cout << fib(6) << endl;
   return 0;
 }
