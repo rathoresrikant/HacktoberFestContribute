021e4409ba0db6c267dd342b30f5c427a19b413d
longdead-ai <shubhampandey28sp@gmail.com>
2019-10-07 05:33:02 +0530

    Added Pernicios Seq in C++

---
+++ printPernicious.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isprime(int n){
+    if(n<2) return false;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if(n%i == 0) return false;
+    }
+    return true; 
+}
+
+void printPernicious(int n) 
+{ 
+    int count = 0;
+    int i = 2;
+    while(count < n){
+        if(isprime(__builtin_popcount(i))){
+            cout << i << " ";
+            count++;
+        }
+        i++;
+    }
+} 
+
+int main(){
+    int n = 25;
+    printPernicious(n);
+}
