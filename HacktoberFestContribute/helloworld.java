16a3d2184a0ac7815d228c77760f4173d60ff706
Gibran <44020043+glgibran7@users.noreply.github.com>
2019-10-27 01:52:30 +0800

    Delete helloworld.java

--- HacktoberFestContribute/helloworld.java
+++
@@ -1,8 +0,0 @@
-ic class HelloWorld {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-
-}
