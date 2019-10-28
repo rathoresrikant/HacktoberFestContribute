e8864816e96d8a1faa225fd9af30c32b0e7e7828
LokiUvaraj <54948079+LokiUvaraj@users.noreply.github.com>
2019-10-28 11:08:45 +0530

    Add files via upload
    Prints Prime numbers

---
+++ Prime.py
@@ -0,0 +1,12 @@
+def prime(end ,start=0):
+	numbers=list(range(start,end+1))
+	result=list()
+	for x in numbers:
+		for y in range(2,x):
+			if x%y==0:
+				break
+			else:
+				result.append(x)
+				break
+	return result
+print(prime(int(input("Enter end number: ")), int(input("Enter start number:"))))
