cbb0fc9e28241222cad4fae65dd7e65d2d849ed2
abdulmk787 <55476136+abdulmk787@users.noreply.github.com>
2019-10-28 13:50:08 +0530

    Simpler solution

--- swap_case.py
+++ swap_case.py
@@ -1,13 +1,9 @@
 str1=input()
-str1=list(str1)
-len1=len(str1)
-for i in range(len1):
-    
-    if (ord(str1[i])>=97 and ord(str1[i])<=122):
-        
-        str1[i]=(str1[i]).upper()
-        
-        
-    elif (ord(str1[i])>=65 and ord(str1[i])<=90):
-        str1[i]=str1[i].lower()
-print("".join(str1))
+s=''
+for i in str1:
+    if str.isupper(i):
+        s=s+str.lower(i)
+    else:
+        s=s+str.upper(i)
+               
+print(s))
