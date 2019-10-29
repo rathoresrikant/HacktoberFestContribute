a6ef46e54d08ce1920e5ab8a4ff3fc4c599e7eff
qmitry <qmitry@gmail.com>
2019-10-29 23:57:14 +0300

    Create yesod.hs

---
+++ yesod.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+import Yesod
+
+data WebApp = WebApp
+
+instance Yesod WebApp
+
+mkYesod "WebApp" [parseRoutes|
+  / HomeR GET
+|]
+
+getHomeR = defaultLayout [whamlet|
+  <div>Exotic!
+|]
+
+main = warpEnv WebApp
