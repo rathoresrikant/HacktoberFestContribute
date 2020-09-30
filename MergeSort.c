c9a556c0342eb63409a40256e10171723c42d1b4
Beeram Rahul Reddy <64956858+rahulbeeram12@users.noreply.github.com>
2020-09-30 21:29:56 +0530

    Update MergeSort.c

--- MergeSort.c
+++ MergeSort.c
@@ -1,45 +1,55 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void merge(int a[11],int low, int mid, int high)
-{
-	int l1,l2,i,b[11];
-	for(l1=low,l2=mid+1,i=low;l1<=mid&&l2<=high;i++)
-	{
-		if(a[l1]<=a[l2])
-			b[i] = a[l1++];
-		else
-			b[i] = a[l2++];
-	}
-	while (l1<=mid)
-		b[i++] = a[l1++];
-	while(l2<=high)
-		b[i++] = a[l2++];
-	for(i=low;i<=high;i++)
-		a[i]=b[i];
-}
-void sort(int a[], int low, int high)
-{ 	
-	int mid;
-	if(low<high)
-	{
-		mid=(low+high)/2;
-		sort(a,low,mid);
-		sort(a,mid+1,high);
-		merge(a,low,mid,high);
-	}
+#define max 10
 
+int a[11] = { 10, 14, 19, 26, 27, 31, 33, 35, 42, 44, 0 };
+int b[10];
+
+void merging(int low, int mid, int high) {
+   int l1, l2, i;
+
+   for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++) {
+      if(a[l1] <= a[l2])
+         b[i] = a[l1++];
+      else
+         b[i] = a[l2++];
+   }
+   
+   while(l1 <= mid)    
+      b[i++] = a[l1++];
+
+   while(l2 <= high)   
+      b[i++] = a[l2++];
+
+   for(i = low; i <= high; i++)
+      a[i] = b[i];
 }
-int main()
-{
-	int i,a[11],n;
-	printf("Enter number of elements:");
-	scanf("%d",&n);
-	printf("Enter elements");
-	for(i=0;i<n;i++)
-		scanf("%d",&a[i]);
-	sort(a,0,n-1);
-	printf("List after sorting:");
-	for(i=0;i<n;i++)
-		printf("%d ",a[i]);
-	return 0;	
+
+void sort(int low, int high) {
+   int mid;
+   
+   if(low < high) {
+      mid = (low + high) / 2;
+      sort(low, mid);
+      sort(mid+1, high);
+      merging(low, mid, high);
+   } else { 
+      return;
+   }   
+}
+
+int main() { 
+   int i;
+
+   printf("List before sorting\n");
+   
+   for(i = 0; i <= max; i++)
+      printf("%d ", a[i]);
+
+   sort(0, max);
+
+   printf("\nList after sorting\n");
+   
+   for(i = 0; i <= max; i++)
+      printf("%d ", a[i]);
 }
