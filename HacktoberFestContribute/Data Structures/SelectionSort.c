3610c18d4b01490b4112f34ae0bba63b951ccaaf
utsav-mishra <54333191+utsav-mishra@users.noreply.github.com>
2020-10-11 07:22:29 +0530

    Update SelectionSort.c

--- HacktoberFestContribute/Data Structures/SelectionSort.c
+++ HacktoberFestContribute/Data Structures/SelectionSort.c
@@ -1,3 +1,11 @@
+//The algorithm maintains two subarrays in a given array.
+
+//1) The subarray which is already sorted.
+//2) Remaining subarray which is unsorted.
+
+//In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray.
+
+
 #include <stdio.h>
  
 int main()
