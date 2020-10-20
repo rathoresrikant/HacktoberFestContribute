f225362859c6f28469f54818236fa833ceb411b9
Gareeb <72250606+Gareeb-coder@users.noreply.github.com>
2020-10-20 14:53:04 +0530

    Update Check_fibonacci.cpp

--- Check_fibonacci.cpp
+++ Check_fibonacci.cpp
@@ -2,14 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-	int n,a;
+	int n;
+	long a;
 	cin>>n;
 	while(n--){
 		cin>>a;
-		if( (ceil(sqrt(5*a*a -4)) == floor(sqrt(5*a*a -4))) || (ceil(sqrt(5*a*a +4)) == floor(sqrt(5*a*a +4))) ){
-		    cout<<1<<endl;
-		}
-		cout<<0<<endl;
+		if( (ceil(sqrt(5*a*a -4)) == floor(sqrt(5*a*a -4))) || (ceil(sqrt(5*a*a +4)) == floor(sqrt(5*a*a +4))) ){ cout<<1<<endl;}
+		else{cout<<0<<endl;}
 	}
 	return 0;
 }
